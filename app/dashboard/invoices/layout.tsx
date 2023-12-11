efc6b2a427af1038ce19fcef66869d35ef3d8dad
Jose Daniel Vasquez <vasquezpinedaj@gmail.com>
2023-12-11 19:43:05 -0400

    feat: :zap: se creo las rutas sin carga

---
+++ app/dashboard/invoices/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <section>
+      Esto es el layout de Invoices
+      {children}
+    </section>
+  );
+}
