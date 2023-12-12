2a285545e4bd5b58dd277af50646cbe60337109d
Jose Daniel Vasquez <vasquezpinedaj@gmail.com>
2023-12-12 01:42:33 -0400

    feat: :zap: se creo carga en paralelo en invoices y revenue

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,12 @@
+import { Suspense } from 'react';
 import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data';
 import { Card } from '../ui/dashboard/cards';
 import LatestInvoices from '../ui/dashboard/latest-invoices';
 import RevenueChart from '../ui/dashboard/revenue-chart';
 import { lusitana } from '../ui/fonts';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '../ui/skeletons';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
   const {
     totalPaidInvoices,
     totalPendingInvoices,
@@ -29,8 +29,12 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
