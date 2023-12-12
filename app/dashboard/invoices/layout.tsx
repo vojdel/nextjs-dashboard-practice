0f7a593bfe49c7723ca814c5c75959dcaee74956
Jose Daniel Vasquez <vasquezpinedaj@gmail.com>
2023-12-12 16:07:14 -0400

    feat: :zap: se modificó invoices y se intalo una nueva dependencia para el search
    invoices e le agrego la tabla y el componente de search, en el componente de search se agrego el use-debounce para que no busque cada vez que presiona una tecla

--- app/dashboard/invoices/layout.tsx
+++ app/dashboard/invoices/layout.tsx
@@ -1,8 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <section>
-      Esto es el layout de Invoices
-      {children}
-    </section>
-  );
+  return <section>{children}</section>;
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -55,6 +55,7 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
