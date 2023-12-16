0c42728b03d18ac53f4133455a0ed85ebf4d760a
Jose Daniel Vasquez <vasquezpinedaj@gmail.com>
2023-12-16 01:23:51 -0400

    feat: :zap: change redirect when into route "/dashboard" and not is logged in

--- auth.config.ts
+++ auth.config.ts
@@ -10,7 +10,7 @@ export const authConfig = {
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+        return Response.redirect(new URL('/', nextUrl)); // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
